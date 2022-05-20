7fa2d5884a9291ff7be2c0346d3f9ba290cd5749
zsszakal <zsszakal@outlook.com>
2022-05-20 21:17:16 +0200

    Add function for shapes to move faster with time, add score

--- main.py
+++ main.py
@@ -211,6 +211,7 @@ def clear_rows(grid, locked):
             if y < ind:
                 newKey = (x, y + inc)
                 locked[newKey] = locked.pop(key)
+    return inc
 
 
 
@@ -230,7 +231,7 @@ def draw_next_shape(shape, surface):
     surface.blit(label, (sx + 10, sy - 30))
 
 
-def draw_window(surface, grid):
+def draw_window(surface, grid, score=0):
     surface.fill((0, 0, 0))
 
     pygame.font.init()
@@ -239,6 +240,14 @@ def draw_window(surface, grid):
 
     # check what this does
     surface.blit(label, (top_left_x + play_width / 2 - (label.get_width() / 2), 30))
+
+    font = pygame.font.SysFont('tahoma', 30)
+    label = font.render('Score: ' + str(score), 1, (255,255,255))
+
+    sx = top_left_x + play_width + 50
+    sy = top_left_y + play_height/2 -100
+
+    surface.blit(label, (sx+10, sy+150))
 
     for i in range(len(grid)):
         for j in range(len(grid[i])):
@@ -260,11 +269,19 @@ def main(win):
     clock = pygame.time.Clock()
     fall_time = 0
     fall_speed = 0.27
+    level_time = 0
+    score = 0
 
     while run:
         grid = create_grid(locked_positions)
         fall_time += clock.get_rawtime()
+        level_time += clock.get_rawtime()
         clock.tick()
+
+        if level_time/1000 > 5:
+            level_time = 0
+            if fall_speed > 0.12:
+                fall_speed -= 0.005
 
         # automatically move the current piece down
         if fall_time/1000 > fall_speed:
@@ -317,9 +334,9 @@ def main(win):
             current_piece = next_piece
             next_piece = get_shape()
             change_piece = False
-            clear_rows(grid, locked_positions)
+            score += clear_rows(grid, locked_positions) * 10
 
-        draw_window(win, grid)
+        draw_window(win, grid, score)
         draw_next_shape(next_piece, win)
         pygame.display.update()
 
